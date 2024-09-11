29103af2bf143b0765dafe68a8fd26e3ae406591
sidmohanty11 <sidmohanty11@gmail.com>
2024-09-11 18:18:27 +0530

    first commit

---
+++ pages/[...app].vue
@@ -0,0 +1,31 @@
+<!-- pages/[...app].vue -->
+
+<script setup>
+import { Content, fetchOneEntry, isPreviewing } from "@builder.io/sdk-vue";
+import { ref } from "vue";
+
+const route = useRoute();
+
+const apiKey = "ad30f9a246614faaa6a03374f83554c9";
+const canShowContent = ref(false);
+const model = "page";
+
+const { data: content } = await useAsyncData("builderData", () =>
+  fetchOneEntry({
+    model,
+    apiKey,
+    userAttributes: {
+      urlPath: route.path,
+    },
+  })
+);
+
+canShowContent.value = content.value ? true : isPreviewing(route.path);
+</script>
+
+<template>
+  <div v-if="canShowContent">
+    <Content :api-key="apiKey" :model="model" :content="content" />
+  </div>
+  <div v-else>Content not Found</div>
+</template>
