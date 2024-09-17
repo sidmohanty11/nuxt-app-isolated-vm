4c577a83031e53f6a25627e30d4ce31731230cbc
sidmohanty11 <sidmohanty11@gmail.com>
2024-09-17 18:36:11 +0530

    try this

--- pages/[...app].vue
+++ pages/[...app].vue
@@ -1,14 +1,15 @@
 <script setup>
 import { Content, fetchOneEntry, isPreviewing } from "@builder.io/sdk-vue";
+if (process.server) {
+  const { initializeNodeRuntime } = await import(
+    "@builder.io/sdk-vue/node/init"
+  );
+  initializeNodeRuntime();
+}
+
 import { ref } from "vue";
 
 const route = useRoute();
-
-// if (import.meta.server) {
-//   const initializeNodeRuntime = (await import("@builder.io/sdk-vue/node/init"))
-//     .initializeNodeRuntime;
-//   initializeNodeRuntime();
-// }
 
 const apiKey = "ad30f9a246614faaa6a03374f83554c9";
 const canShowContent = ref(false);
--- plugins/builder-init.ts
+++ plugins/builder-init.ts
@@ -1,13 +1,11 @@
-// import * as builderInit from "@builder.io/sdk-vue/node/init";
-
 export default defineNuxtPlugin((nuxtApp) => {
-  if (process.server) {
-    async function importIsolatedVM() {
-      const initializeNodeRuntime = (
-        await import("@builder.io/sdk-vue/node/init")
-      ).initializeNodeRuntime;
-      initializeNodeRuntime();
-    }
-    importIsolatedVM();
-  }
+  // if (process.server) {
+  //   async function importIsolatedVM() {
+  //     const initializeNodeRuntime = (
+  //       await import("@builder.io/sdk-vue/node/init")
+  //     ).initializeNodeRuntime;
+  //     initializeNodeRuntime();
+  //   }
+  //   importIsolatedVM();
+  // }
 });
