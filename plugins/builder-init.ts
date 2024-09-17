aa9bc7d475d0feed9cab72831f6570dd43d968af
sidmohanty11 <sidmohanty11@gmail.com>
2024-09-17 21:04:02 +0530

    ok for initial hmr its throwing error

--- plugins/builder-init.ts
+++ plugins/builder-init.ts
@@ -1,5 +1,12 @@
 export default defineNuxtPlugin((nuxtApp) => {
-  if (process.server) {
+  nuxtApp.vueApp.config.warnHandler = (msg, vm, trace) => {
+    if (msg.includes("Extraneous non-props attributes")) {
+      return;
+    }
+    console.warn(msg, vm, trace);
+  };
+  if (process.server || import.meta.server) {
+    console.log("here");
     async function importIsolatedVM() {
       const { initializeNodeRuntime } = await import(
         "@builder.io/sdk-vue/node/init"
