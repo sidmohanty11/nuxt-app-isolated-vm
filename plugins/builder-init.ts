35df5aa32f0877352d90bc42367ee7d33f1e58ae
sidmohanty11 <sidmohanty11@gmail.com>
2024-09-17 19:29:47 +0530

    change

--- plugins/builder-init.ts
+++ plugins/builder-init.ts
@@ -1,11 +1,11 @@
 export default defineNuxtPlugin((nuxtApp) => {
-  // if (process.server) {
-  //   async function importIsolatedVM() {
-  //     const initializeNodeRuntime = (
-  //       await import("@builder.io/sdk-vue/node/init")
-  //     ).initializeNodeRuntime;
-  //     initializeNodeRuntime();
-  //   }
-  //   importIsolatedVM();
-  // }
+  if (process.server) {
+    async function importIsolatedVM() {
+      const initializeNodeRuntime = (
+        await import("@builder.io/sdk-vue/node/init")
+      ).initializeNodeRuntime;
+      initializeNodeRuntime();
+    }
+    importIsolatedVM();
+  }
 });
