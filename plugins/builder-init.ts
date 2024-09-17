987828629372994ca45757073fa931fc4f10e5da
sidmohanty11 <sidmohanty11@gmail.com>
2024-09-17 19:39:09 +0530

    change

--- plugins/builder-init.ts
+++ plugins/builder-init.ts
@@ -1,9 +1,9 @@
 export default defineNuxtPlugin((nuxtApp) => {
   if (process.server) {
     async function importIsolatedVM() {
-      const initializeNodeRuntime = (
-        await import("@builder.io/sdk-vue/node/init")
-      ).initializeNodeRuntime;
+      const { initializeNodeRuntime } = await import(
+        "@builder.io/sdk-vue/node/init"
+      );
       initializeNodeRuntime();
     }
     importIsolatedVM();
