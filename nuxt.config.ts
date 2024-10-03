8abd3da9f0886cde3cfd965e16fc8a1d0fd9390d
sidmohanty11 <sidmohanty11@gmail.com>
2024-10-03 22:54:59 +0530

    try deploy

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,14 +2,23 @@
 export default defineNuxtConfig({
   compatibilityDate: "2024-04-03",
   devtools: { enabled: true },
-  modules: ["@builder.io/sdk-vue/nuxt"],
-  plugins: ["@/plugins/builder-init"],
+  modules: [
+    [
+      "@builder.io/sdk-vue/nuxt",
+      {
+        includeCompiledCss: true,
+        initializeNodeRuntime: true,
+      },
+    ],
+    // "@/modules/test-module",
+  ],
+  // plugins: ["@/plugins/builder-init"],
   // vite: {
   //   optimizeDeps: {
   //     exclude: ["@builder.io/sdk-vue/node/init"],
   //   },
   // },
   // build: {
-  //   transpile: ["@builder.io/sdk-vue"],
+  //   transpile: ["@builder.io/sdk-vue/node/init"],
   // },
 });
