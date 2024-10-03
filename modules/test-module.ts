0240d061604c6ac9f17690dc7262e87ce76f6f77
sidmohanty11 <sidmohanty11@gmail.com>
2024-10-03 23:01:11 +0530

    comment

--- modules/test-module.ts
+++ modules/test-module.ts
@@ -1,44 +1,44 @@
-import { addPlugin, defineNuxtModule } from "@nuxt/kit";
+// import { addPlugin, defineNuxtModule } from "@nuxt/kit";
 
-interface Options {
-  includeCompiledCss: boolean;
-  initializeNodeRuntime: boolean;
-}
+// interface Options {
+//   includeCompiledCss: boolean;
+//   initializeNodeRuntime: boolean;
+// }
 
-export default defineNuxtModule<Options>({
-  setup(options, nuxt) {
-    const includeCompiledCss = options.includeCompiledCss ?? true;
-    const initializeNodeRuntime = options.initializeNodeRuntime ?? false;
-    /**
-     * Add the compiled Builder.io CSS to the Nuxt CSS array.
-     */
-    if (includeCompiledCss) {
-      nuxt.options.css.push("@builder.io/sdk-vue/css");
-    }
-    /**
-     * This is because Vite tries to optimize the isolated-vm dependency while
-     * running the dev server (first build), which is not needed and throws an error.
-     * `isolated-vm` is a Node.js native module which should only be imported and used in Node.js environments.
-     */
-    if (initializeNodeRuntime) {
-      if (nuxt.options.vite?.optimizeDeps?.exclude) {
-        nuxt.options.vite.optimizeDeps.exclude.push(
-          "@builder.io/sdk-vue/node/init"
-        );
-      } else {
-        nuxt.options.vite = {
-          ...nuxt.options.vite,
-          optimizeDeps: {
-            ...nuxt.options.vite?.optimizeDeps,
-            exclude: ["@builder.io/sdk-vue/node/init"],
-          },
-        };
-      }
+// export default defineNuxtModule<Options>({
+//   setup(options, nuxt) {
+//     const includeCompiledCss = options.includeCompiledCss ?? true;
+//     const initializeNodeRuntime = options.initializeNodeRuntime ?? false;
+//     /**
+//      * Add the compiled Builder.io CSS to the Nuxt CSS array.
+//      */
+//     if (includeCompiledCss) {
+//       nuxt.options.css.push("@builder.io/sdk-vue/css");
+//     }
+//     /**
+//      * This is because Vite tries to optimize the isolated-vm dependency while
+//      * running the dev server (first build), which is not needed and throws an error.
+//      * `isolated-vm` is a Node.js native module which should only be imported and used in Node.js environments.
+//      */
+//     if (initializeNodeRuntime) {
+//       if (nuxt.options.vite?.optimizeDeps?.exclude) {
+//         nuxt.options.vite.optimizeDeps.exclude.push(
+//           "@builder.io/sdk-vue/node/init"
+//         );
+//       } else {
+//         nuxt.options.vite = {
+//           ...nuxt.options.vite,
+//           optimizeDeps: {
+//             ...nuxt.options.vite?.optimizeDeps,
+//             exclude: ["@builder.io/sdk-vue/node/init"],
+//           },
+//         };
+//       }
 
-      addPlugin({
-        src: "./plugins/nuxt-isolated-vm-plugin.js",
-        mode: "server",
-      });
-    }
-  },
-});
+//       addPlugin({
+//         src: "./plugins/nuxt-isolated-vm-plugin.js",
+//         mode: "server",
+//       });
+//     }
+//   },
+// });
--- plugins/builder-init.ts
+++ plugins/builder-init.ts
@@ -1,18 +1,18 @@
-export default defineNuxtPlugin((nuxtApp) => {
-  nuxtApp.vueApp.config.warnHandler = (msg, vm, trace) => {
-    if (msg.includes("Extraneous non-props attributes")) {
-      return;
-    }
-    console.warn(msg, vm, trace);
-  };
-  // correctly initialize Isolated VM
-  if (process.server || import.meta.server) {
-    async function importIsolatedVM() {
-      const { initializeNodeRuntime } = await import(
-        "@builder.io/sdk-vue/node/init"
-      );
-      initializeNodeRuntime();
-    }
-    importIsolatedVM();
-  }
-});
+// export default defineNuxtPlugin((nuxtApp) => {
+//   nuxtApp.vueApp.config.warnHandler = (msg, vm, trace) => {
+//     if (msg.includes("Extraneous non-props attributes")) {
+//       return;
+//     }
+//     console.warn(msg, vm, trace);
+//   };
+//   // correctly initialize Isolated VM
+//   if (process.server || import.meta.server) {
+//     async function importIsolatedVM() {
+//       const { initializeNodeRuntime } = await import(
+//         "@builder.io/sdk-vue/node/init"
+//       );
+//       initializeNodeRuntime();
+//     }
+//     importIsolatedVM();
+//   }
+// });
