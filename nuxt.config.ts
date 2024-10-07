e09b59f7fd28555cc4ef6f497ad8bb1d8707336b
sidmohanty11 <sidmohanty11@gmail.com>
2024-10-07 10:17:54 +0530

    push

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,15 +2,15 @@
 export default defineNuxtConfig({
   compatibilityDate: "2024-04-03",
   devtools: { enabled: true },
-  modules: [
-    [
-      "@builder.io/sdk-vue/nuxt",
-      {
-        includeCompiledCss: true,
-        initializeNodeRuntime: true,
-      },
-    ],
-  ],
+  // modules: [
+  //   [
+  //     "@builder.io/sdk-vue/nuxt",
+  //     {
+  //       includeCompiledCss: true,
+  //       initializeNodeRuntime: true,
+  //     },
+  //   ],
+  // ],
   // plugins: ["@/plugins/builder-init"],
   // vite: {
   //   optimizeDeps: {
